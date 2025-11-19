56879dcbc5d4b74a73edd4759bf4b0fb61022523
Dardeka <ddkwari2@gmail.com>
2025-11-19 16:48:15 +0700

    fix: Correct background image path in ComingSoon component

--- frontend/src/ComingSoon.jsx
+++ frontend/src/ComingSoon.jsx
@@ -1,6 +1,6 @@
 function ComingSoon(){
     return(
-        <div className="w-screen h-screen bg-[url(/public/images/background.png)] flex flex-col items-center justify-center">
+        <div className="w-screen h-screen bg-[url(/images/background.png)] flex flex-col items-center justify-center">
             <img src="/logo/logoTeksOnly.png" alt="" width='520px' />
             <p className="text-3xl">Stay Tuned! We're almost ready to wrap <br/>  your sweet memories</p>
         </div>
